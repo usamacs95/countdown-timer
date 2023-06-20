fa5fe9d6ca656cfa42e37fde078c95220d5d942b
Usama Qazi <usama_cs95@outlook.com>
2023-06-20 19:01:45 +0500

    timer working on console

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -3,8 +3,12 @@ const newYears = "1 Jan 2024";
 function countdown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
+  const seconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(seconds / 3600 / 24);
+  const hours = Math.floor(seconds / 3600) % 24;
+  const minutes = Math.floor(seconds / 60) % 60;
 
-  console.log(newYearsDate - currentDate);
+  console.log(days, hours, minutes);
 }
 countdown();
 setInterval(countdown, 1000);
