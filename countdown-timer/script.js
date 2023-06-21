2131ecdb8c89c8be8f8d3b79ef52637c53843ff4
Usama Qazi <usama_cs95@outlook.com>
2023-06-21 14:12:38 +0500

    Added proper zero formating on digits

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -16,9 +16,12 @@ function countdown() {
   const seconds = Math.floor(totalSeconds) % 60;
 
   daysEl.innerHTML = days;
-  hoursEl.innerHTML = hours;
-  minsEl.innerHTML = mins;
-  secondsEl.innerHTML = seconds;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(mins);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
 }
 countdown();
 setInterval(countdown, 1000);
