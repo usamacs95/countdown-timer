f996339032f0abcea8685cafaf95fd73d6b843ae
Usama Qazi <usama_cs95@outlook.com>
2023-06-21 13:48:47 +0500

    time showing and changing properly

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,14 +1,24 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
 const newYears = "1 Jan 2024";
 
 function countdown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
-  const seconds = (newYearsDate - currentDate) / 1000;
-  const days = Math.floor(seconds / 3600 / 24);
-  const hours = Math.floor(seconds / 3600) % 24;
-  const minutes = Math.floor(seconds / 60) % 60;
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  console.log(days, hours, minutes);
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secondsEl.innerHTML = seconds;
 }
 countdown();
 setInterval(countdown, 1000);
